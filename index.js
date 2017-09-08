a307bbf623297dcf96963353362d7037c22d3157
Curtis Ekstrom <code@fauxton.io>
2017-09-08 13:02:43 +0200

    Only register plugin to run on parent

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ module.exports = {
   name: 'ember-dom-inventory',
 
   setupPreprocessorRegistry(type, registry) {
-    var DomInventory = require('./lib/dom-inventory');
+    if (type === 'parent') {
+      var DomInventory = require('./lib/dom-inventory');
 
-    registry.add('htmlbars-ast-plugin', {
-      name: 'inventory-dom-elements-and-classes',
-      plugin: DomInventory,
-      baseDir: function() { return __dirname; }
-    });
+      registry.add('htmlbars-ast-plugin', {
+        name: 'ember-dom-inventory',
+        plugin: DomInventory,
+        baseDir: function() { return __dirname; }
+      });
+    }
   },
+
+  postBuild(result) {
+    var exec = require('child_process').exec;
+    function puts(error, stdout, stderr) { console.log(stdout); }
+    exec("rm -rf ./node-persist", puts);
+    return result;
+  }
 };
